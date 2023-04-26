a53ebd1d09228cefc5b083b5726f005e9e693f52
LiamMoquin <Liammoquin06@gmail.com>
2023-04-25 17:50:00 -0700

    testing contact page

---
+++ send_email.php
@@ -0,0 +1,19 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$to = "Liammoquin06@gmail.com"; // replace with your own email address
+$subject = $_POST['subject'];
+$content = $_POST['content'];
+$headers = "From: ".$_POST['email']."\r\n";
+$headers .= "Reply-To: ".$_POST['email']."\r\n";
+
+// Send email
+mail($to, $subject, $content, $headers);
+
+// Redirect back to Contact.html
+header('Location: Contact.html');
+exit;
+?>
